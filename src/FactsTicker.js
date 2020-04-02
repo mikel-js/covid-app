2377811c916dcd792990c161c1d23c2ccc5e6b92
mikel-js <michaelcastro.js@gmail.com>
2020-04-02 22:29:40 +0300

    ticker

--- src/FactsTicker.js
+++ src/FactsTicker.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Ticker from 'react-ticker';
 
+
 function FactsTicker() {
-  const updates = [
+    const updates = [
     { info: "COVID-19's outer layer is made up of lipids(fats), that's why washing with soap is best recommended, it dissolves the virus' protective layer. Way to go SOAP!" },
     { info: "Antibiotics don't work on COVID, as it is caused by a corona virus" }
   ]
   return (
     <>
       <h3 style={{ color: 'black' }}>Did you know?</h3>
-      <Ticker mode='await' speed={10} >
+      <Ticker mode='await' speed={15} offset="run-in" style={{width: '100%', overflow: 'hidden'}}>
         {() => (
           <>
             <h5>
@@ -23,6 +24,5 @@ function FactsTicker() {
     </>
   )
 }
+
 export default FactsTicker
-
-
